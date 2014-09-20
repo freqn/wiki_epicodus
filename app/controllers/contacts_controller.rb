cede195d14d4d1ab4068ac13e1a01585f144a3a0
c <freqnlabs@gmail.com>
2014-09-20 15:39:05 -0400

    Update create, update, destroy to redirects

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,49 @@
+class ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all
+    render('contacts/index')
+  end
+
+  def new
+    @contact = Contact.new
+    render('contacts/new')
+  end
+
+  def create
+    @contact = Contact.new(:name => params[:name],
+                           :email => params[:email],
+                           :phone => params[:phone])
+    if @contact.save
+      redirect_to("/contacts/#{@contact.id}")
+    else
+      render('contacts/new')
+    end
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
+    render('contacts/show')
+  end
+
+  def edit
+    @contact = Contact.find(params[:id])
+    render('contacts/edit')
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+    if @contact.update(:name => params[:name],
+                        :email => params[:email],
+                          :phone => params[:phone])
+      redirect_to("/contacts/#{@contact.id}")
+    else
+      render('contacts/edit')
+    end
+  end
+
+  def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+    redirect_to("/contacts")
+  end
+end
